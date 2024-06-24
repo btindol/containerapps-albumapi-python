1eccae2347b253c9a49d1a6de70c57a5eb4d6bb4
btindol <blake.tindol@stryker.com>
2024-06-24 14:30:52 -0400

    flask containerized branch

--- src/app/main.py
+++ src/app/main.py
@@ -1,40 +1,40 @@
-from typing import Optional
+from flask import Flask, jsonify
+from flask_cors import CORS
 
-from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
+app = Flask(__name__)
+CORS(app)
 
-app = FastAPI()
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods="GET",
-    allow_headers=["*"]
-)
-
-class Album():
+class Album:
     def __init__(self, id, title, artist, price, image_url):
-         self.id = id
-         self.title = title
-         self.artist = artist
-         self.price = price
-         self.image_url = image_url
+        self.id = id
+        self.title = title
+        self.artist = artist
+        self.price = price
+        self.image_url = image_url
 
-albums = [ 
+albums = [
     Album(1, "You, Me and an App Id", "Daprize", 10.99, "https://aka.ms/albums-daprlogo"),
     Album(2, "Seven Revision Army", "The Blue-Green Stripes", 13.99, "https://aka.ms/albums-containerappslogo"),
     Album(3, "Scale It Up", "KEDA Club", 13.99, "https://aka.ms/albums-kedalogo"),
-    Album(4, "Lost in Translation", "MegaDNS", 12.99,"https://aka.ms/albums-envoylogo"),
+    Album(4, "Lost in Translation", "MegaDNS", 12.99, "https://aka.ms/albums-envoylogo"),
     Album(5, "Lock Down Your Love", "V is for VNET", 12.99, "https://aka.ms/albums-vnetlogo"),
     Album(6, "Sweet Container O' Mine", "Guns N Probeses", 14.99, "https://aka.ms/albums-containerappslogo")
 ]
 
-@app.get("/")
+@app.route("/")
 def read_root():
-    return {"Access /albums to see the list of albums"}
+    return jsonify({"message": "Access /albums to see the list of albums"})
 
-
-@app.get("/albums")
+@app.route("/albums")
 def get_albums():
-    return albums
+    albums_list = [{
+        "id": album.id,
+        "title": album.title,
+        "artist": album.artist,
+        "price": album.price,
+        "image_url": album.image_url
+    } for album in albums]
+    return jsonify(albums_list)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
