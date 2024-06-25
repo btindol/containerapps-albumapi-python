4f948c289354b642da1eda9e2de46b51ab25cb48
btindol <blake.tindol@stryker.com>
2024-06-25 09:50:51 -0400

    push1

--- README.md
+++ README.md
@@ -1,6 +1,59 @@
-# Azure Container Apps Album API
-1) Create all resources except the container app and pushing the image to acr.
-2) Make github actions that runs the az commands?
-3) Check setup.txdt
 
+##############################################################################################################################
+# Create resources for flask container app and make github actions in flask repo to push image to acr and deploy to container app
+###########################
+1) run the terraform init, plan and apply on main tf (in terraform directory)
+2) go to flask app repo https://github.com/btindol/flask_template.git
+3) make service principle az login then
+    - az ad sp create-for-rbac --name "myServicePrincipal" --role Contributor --scopes /subscriptions/{subscription-id}/resourceGroups/{resource-group-name}
+4) take the json output and maek a github secret in new secret enviornment (environment: container-app-deploy) AZURE_CREDENTIALS
+5) add the .github/workflows/deploy.yml` file which is below.
+
+
+
+
+1. **Create `.github/workflows/deploy.yml`**: In your Flask app repository, create the following file:
+
+```yaml
+name: Deploy to Azure
+
+on:
+  push:
+    branches:
+      - main
+      - terraform-flask-deployment
+
+jobs:
+  build-and-deploy:
+    runs-on: ubuntu-latest
+    environment: container-app-deploy
+
+    steps:
+    - name: Checkout code
+      uses: actions/checkout@v2
+
+    - name: Set up Azure CLI
+      uses: azure/CLI@v1
+      with:
+        inlineScript: |
+          echo "Azure CLI setup complete"
+
+    - name: Login to Azure
+      uses: azure/login@v1
+      with:
+        creds: ${{ secrets.AZURE_CREDENTIALS }}
+
+    - name: Build and push Docker image
+      run: |
+        az acr build --registry acaalbumsbtindol --image album-api .
+
+    - name: Create container app environment
+      run: |
+        az containerapp env create --name env-album-containerapps --resource-group album-containerapps --location canadacentral
+
+    - name: Deploy image to container app
+      run: |
+        az containerapp create --name album-api --resource-group album-containerapps --environment env-album-containerapps --image "acaalbumsbtindol.azurecr.io/album-api" --target-port 8080 --ingress external --registry-server "acaalbumsbtindol.azurecr.io"
+
+```
 
--- src/terraform/main.tf
+++ src/terraform/main.tf
@@ -52,70 +52,70 @@ resource "azurerm_container_app_environment" "main" {
   location            = var.location
 }
 
-# Build and push Docker image
-resource "null_resource" "build_and_push_image" {
-  provisioner "local-exec" {
-    command = <<EOT
-      echo "Current working directory:"
-      cd
-      echo "Listing contents of working directory:"
-      dir
-      echo "Checking if Dockerfile exists:"
-      if exist Dockerfile (
-        echo "Dockerfile found"
-      ) else (
-        echo "Dockerfile not found"
-      )
-      echo "Running az acr build command"
-      az acr build --registry ${azurerm_container_registry.main.name} --image ${var.api_name}:latest .
-      echo "Build command completed"
-    EOT
-    working_dir = "C:\\temp\\python_container_app\\containerapps-albumapi-python\\src"  # Adjusted to full path
-  }
-  depends_on = [azurerm_container_registry.main]
-}
+# # Build and push Docker image
+# resource "null_resource" "build_and_push_image" {
+#   provisioner "local-exec" {
+#     command = <<EOT
+#       echo "Current working directory:"
+#       cd
+#       echo "Listing contents of working directory:"
+#       dir
+#       echo "Checking if Dockerfile exists:"
+#       if exist Dockerfile (
+#         echo "Dockerfile found"
+#       ) else (
+#         echo "Dockerfile not found"
+#       )
+#       echo "Running az acr build command"
+#       az acr build --registry ${azurerm_container_registry.main.name} --image ${var.api_name}:latest .
+#       echo "Build command completed"
+#     EOT
+#     working_dir = "C:\\temp\\python_container_app\\containerapps-albumapi-python\\src"  # Adjusted to full path
+#   }
+#   depends_on = [azurerm_container_registry.main]
+# }
 
-# Deploy your image to container app
-resource "azurerm_container_app" "api" {
-  name                        = var.api_name
-  container_app_environment_id = azurerm_container_app_environment.main.id
-  resource_group_name         = azurerm_resource_group.main.name
-  revision_mode               = "Multiple"
+# # Deploy your image to container app
+# resource "azurerm_container_app" "api" {
+#   name                        = var.api_name
+#   container_app_environment_id = azurerm_container_app_environment.main.id
+#   resource_group_name         = azurerm_resource_group.main.name
+#   revision_mode               = "Multiple"
 
-  ingress {
-    external_enabled = true
-    target_port      = 8080
-    traffic_weight {
-      percentage      = 100
-      latest_revision = true
-    }
-  }
+#   ingress {
+#     external_enabled = true
+#     target_port      = 8080
+#     traffic_weight {
+#       percentage      = 100
+#       latest_revision = true
+#     }
+#   }
 
-  template {
-    container {
-      name   = var.api_name
-      image  = "${azurerm_container_registry.main.login_server}/${var.api_name}:latest"
-      cpu    = "0.5"
-      memory = "1.0Gi"
-    }
-  }
+#   template {
+#     container {
+#       name   = var.api_name
+#       image  = "${azurerm_container_registry.main.login_server}/${var.api_name}:latest"
+#       cpu    = "0.5"
+#       memory = "1.0Gi"
+#     }
+#   }
 
-  identity {
-    type = "SystemAssigned"
-  }
+#   identity {
+#     type = "SystemAssigned"
+#   }
 
-  depends_on = [null_resource.build_and_push_image]
-}
+#   depends_on = [null_resource.build_and_push_image]
+# }
 
 # Assign ACR Pull role to the container app
-resource "azurerm_role_assignment" "acr_pull" {
-  principal_id         = azurerm_container_app.api.identity[0].principal_id
-  role_definition_name = "AcrPull"
-  scope                = azurerm_container_registry.main.id
-  depends_on           = [azurerm_container_app.api]
-}
+# resource "azurerm_role_assignment" "acr_pull" {
+#   principal_id         = azurerm_container_app.api.identity[0].principal_id
+#   role_definition_name = "AcrPull"
+#   scope                = azurerm_container_registry.main.id
+#   depends_on           = [azurerm_container_app.api]
+# }
 
-# Output the FQDN of the container app
-output "app_fqdn" {
-  value = azurerm_container_app.api.latest_revision_fqdn
-}
+# # Output the FQDN of the container app
+# output "app_fqdn" {
+#   value = azurerm_container_app.api.latest_revision_fqdn
+# }
